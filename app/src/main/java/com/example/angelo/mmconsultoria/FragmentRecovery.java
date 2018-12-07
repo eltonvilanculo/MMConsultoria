5119796db267c13fab9396ba994486080378b93b
PosterApp <eltonvillas10@gmail.com>
2018-12-07 11:58:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/angelo/mmconsultoria/FragmentRecovery.java
@@ -0,0 +1,48 @@
+package com.example.angelo.mmconsultoria;
+
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentRecovery extends Fragment {
+ImageButton backButton;
+
+    public FragmentRecovery() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_recovery, container, false);
+        backButton  = view.findViewById(R.id.button3);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container,new BlankFragment()).commit();
+            }
+        });
+
+        return view;
+    }
+
+
+
+}
